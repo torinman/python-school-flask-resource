09026935aa3b856735342e0521ee82d240e0056d
torinman <77184911+torinman@users.noreply.github.com>
2023-03-28 11:20:59 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+    return flask.render_template("index.html")
+
+@app.route('/medicine')
+def med():
+    return flask.render_template("medicine.html")
+
+@app.route('/jobs')
+def job():
+    return flask.render_template("jobs.html")
